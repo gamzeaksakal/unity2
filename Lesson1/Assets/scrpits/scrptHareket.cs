4eb11ee600e498326d330fe908b13132dc4e7696
gamzeaksakal <gamzeaksakal@users.noreply.github.com>
2019-01-12 12:02:49 +0300

    data
    ...

--- Lesson1/Assets/scrpits/scrptHareket.cs
+++ Lesson1/Assets/scrpits/scrptHareket.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HareketController : MonoBehaviour
+public class scrptHareket : MonoBehaviour
 {
     public int speed = 0;
     Rigidbody rBody;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(string.Format("x: {0}", gameObject.transform.position.x.ToString()));
-        Debug.Log(string.Format("y: {0}", gameObject.transform.position.y.ToString()));
-        Debug.Log(string.Format("z: {0}", gameObject.transform.position.z.ToString()));
-        rBody = gameObject.GetComponent<Rigidbody>();
-
+        Debug.Log(string.Format("x : {0}", gameObject.transform.position.x.ToString()));
+        Debug.Log(string.Format("y : {0}", gameObject.transform.position.y.ToString()));
+        Debug.Log(string.Format("z : {0}", gameObject.transform.position.z.ToString()));
+        rBody = gameObject.GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -29,30 +27,31 @@ public class HareketController : MonoBehaviour
         {
             rBody.AddForce(0, 0, -speed);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.D))
         {
             rBody.AddForce(speed, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.A))
         {
             rBody.AddForce(-speed, 0, 0);
         }
-        //// if(Input.GetAxis("Horizontal") < 0.0)
-        //{
-        // gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + speed);
-        //    gameObject.transform.position = new Vector3(gameObject.transform.position.x - speed, gameObject.transform.position.y, gameObject.transform.position.z);
-        //}
-        //else
-        //{
-        //    gameObject.transform.position = new Vector3(gameObject.transform.position.x + speed, gameObject.transform.position.y, gameObject.transform.position.z);
-        //}
-        //if (Input.GetAxis("Vertical") < 0.0)
-        //{
-        //    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - speed);
-        //}
-        //else
-        //{
-        //    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + speed);
-        //}//
+
+        /*
+         if(Input.GetAxis("Horizontal") < 0.0) //left
+         {
+             gameObject.transform.position = new Vector3((gameObject.transform.position.x - speed) * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+
+         } else //rigth
+         {
+             gameObject.transform.position = new Vector3((gameObject.transform.position.x + speed) * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+         }
+         if (Input.GetAxis("Vertical") < 0.0) // down
+         {
+             gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, (gameObject.transform.position.z - speed) * Time.deltaTime);
+         } else //up
+         {
+             gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, (gameObject.transform.position.z + speed) * Time.deltaTime);
+         }
+         */
     }
 }
